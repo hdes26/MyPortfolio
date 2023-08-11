c738408a23c396cca8bcf17ca87d2135bf67a9da
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-11 12:06:45 -0500

    [ADD] navbar :alien:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+import { Navbar } from '@/components/Navbar'
+
 export default function Home () {
   return (
     <>
-      <h1>Init</h1>
+      <Navbar />
       <section
         id='about-me'
         className='grid grid-cols-3 h-max gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20 z-10 fade-in-from-top'

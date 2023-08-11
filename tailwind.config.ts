27b0b90feeb78a5dcb314108e775dbf7b4968af1
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-11 11:39:18 -0500

    [UPDATE] tailwind config :boom:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,30 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        shrink: 'shrink 1s infinite',
+        shrinkAlternative: 'shrinkAlternative 1s infinite'
       },
-    },
+      keyframes: {
+        shrink: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(0.4)' },
+          '75%': { transform: 'scale(0.8)' },
+          '100%': { transform: 'scale(1)' }
+        },
+        shrinkAlternative: {
+          '20%': { transform: 'scale(1)' },
+          '15%': { transform: 'scale(0.6)' },
+          '10%': { transform: 'scale(0.8)' },
+          '0%': { transform: 'scale(1)' }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config

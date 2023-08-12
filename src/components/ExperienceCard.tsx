b6145ee63843a97aab150142506216133742a6cb
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-11 23:54:57 -0500

    [FIX] error in vercel

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,6 +1,4 @@
-import { calculateYearsOfExperience } from '@/utils/yearsOfExperience'
 import { SelectContainer } from './containers'
-import { CountUp } from 'use-count-up'
 
 export const ExperienceCard: React.FC = () => {
   return (
@@ -12,9 +10,7 @@ export const ExperienceCard: React.FC = () => {
             className={`${currentSelect === 'home' || currentSelect === 'about' ? 'opacity-100' : ' opacity-20'} text-white rounded-3xl flex relative overflow-hidden aspect-square shadow-sm bg-blue-400/80 p-4 justify-center items-center`}
           >
             <div className='flex justify-center items-center flex-col'>
-              <p className='text-4xl md:text-8xl font-bold'>
-                +<CountUp isCounting end={calculateYearsOfExperience()} duration={3} />
-              </p>
+              <p className='text-4xl md:text-8xl font-bold'>+2</p>
               <p className='text-xs md:text-xl uppercase font-semibold text-center'>years of experience</p>
             </div>
           </div>
